fa84d0d6730170dfef5a5131a6c852baa8f6ea89
Milan May'r <mayrmilan@gmail.com>
2020-10-24 11:36:22 -0500

    added basic comments

--- project.py
+++ project.py
@@ -11,6 +11,7 @@ def subtract(x, y):
     return x - y
 
 def calculate(first_number, second_number, operator):
+    # This function creates a result based on the math operator that is being passed in
     if operator == '+':
         return add(first_number, second_number)
     if operator == '-':
@@ -21,11 +22,14 @@ def calculate(first_number, second_number, operator):
         return divide(first_number, second_number)
 
 if __name__ == '__main__':
+    # Introduce app
     print("This app is a super basic calculator.")
 
+    # Prompt for first number
     print("\nEnter the first number: ")
     first_number = int(input())
 
+    # Explain operator options, then prompt for operator
     print("\n----")
     print("----")
     print(" + for addition")
@@ -37,9 +41,12 @@ if __name__ == '__main__':
     print("Enter an operator: ")
     operator = input()
 
+    # Prompt for second number
     print("\nEnter the second number: ")
     second_number = int(input())
 
+    # Calculate result
     result = calculate(first_number, second_number, operator)
 
+    # Show result
     print('\n' + str(first_number) + ' ' + operator + ' ' + str(second_number) + ' = ' + str(result))
